4f4bc62c5a553320162d41f76d8b55be0db95644
Limwonbin <wonbin3366@naver.com>
2023-02-09 16:48:06 +0900

    게시글 목록보기 레파지토리 테스트 완료

--- src/main/java/shop/mtcoding/blog/model/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/model/BoardRepository.java
@@ -5,17 +5,21 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import shop.mtcoding.blog.dto.board.BoardResp.BoardMainRespDto;
+
 @Mapper
 public interface BoardRepository {
-    public List<Board> findAll();
+        public List<Board> findAll();
 
-    public Board findById(int id);
+        public List<BoardMainRespDto> findAllWithUser();
 
-    public int insert(@Param("title") String title, @Param("content") String content,
-            @Param("userId") int userId);
+        public Board findById(int id);
 
-    public int updateById(@Param("id") int id, @Param("title") String title,
-            @Param("content") String content);
+        public int insert(@Param("title") String title, @Param("content") String content,
+                        @Param("userId") int userId);
 
-    public int deleteById(int id);
+        public int updateById(@Param("id") int id, @Param("title") String title,
+                        @Param("content") String content);
+
+        public int deleteById(int id);
 }
