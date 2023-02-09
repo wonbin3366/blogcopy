0d84db6e527145895683dadbc47c9bd3dbe7475b
Limwonbin <wonbin3366@naver.com>
2023-02-09 16:57:02 +0900

    게시글 목록보기 완료

--- src/main/java/shop/mtcoding/blog/service/BoardService.java
+++ src/main/java/shop/mtcoding/blog/service/BoardService.java
@@ -3,17 +3,20 @@ package shop.mtcoding.blog.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import shop.mtcoding.blog.dto.board.BoardReq.BoardSaveReqDto;
 import shop.mtcoding.blog.handler.ex.CustomException;
 import shop.mtcoding.blog.model.BoardRepository;
 
+@Transactional(readOnly = true)
 @Service
 public class BoardService {
 
     @Autowired
     private BoardRepository boardRepository;
 
+    @Transactional
     public void 글쓰기(BoardSaveReqDto boardSaveReqDto, int userId) {
         int result = boardRepository.insert(boardSaveReqDto.getTitle(), boardSaveReqDto.getContent(), userId);
         if (result != 1) {
